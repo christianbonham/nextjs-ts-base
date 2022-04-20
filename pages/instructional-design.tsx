4c26141094a54d4bba5935037410c1d6be251d44
Christian Bonham <cbonham@kcstardust.com>
2022-04-20 10:10:49 -0700

    first commit

---
+++ pages/instructional-design.tsx
@@ -0,0 +1,7 @@
+
+
+export default function InstructionalDesign() {
+    return (
+        <h1>Instructional Design</h1>
+    );
+};
---
+++ pages/ux-development.tsx
@@ -0,0 +1,5 @@
+export default function UXDevelopment() {
+    return (
+        <h1>UX Development</h1>
+    )
+}
