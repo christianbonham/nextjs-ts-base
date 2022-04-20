82ddf7048fb60ed98d422c7eab5c3c975d6690c4
Christian Bonham <cbonham@kcstardust.com>
2022-04-20 13:22:10 -0700

    disable eslint props spreading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,6 @@ import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
